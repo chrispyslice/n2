4e2843863dbfa26b32549e77edb7b80437ae898b
chrispyslice <granville.chris@googlemail.com>
2011-06-20 16:30:34 +0100

    Partial initial file structure

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * N2
+ * 
+ * @license		CC BY-SA 3.0
+ * @author		Chris Atkin
+ * @link		http://chrisatk.in/
+ * @email		contact {at} chrisatk {dot} in
+ * 
+ * @file		index.php
+ * @version		1.0
+ * @date		05/26/2011
+ * 
+ * Copyright (c) 2011 Chris Atkin. All rights reserved.
+ */
+
+// --------------------------------------------------------------------------
+ 
+namespace n2;
+ 
+/**
+ * Start error reporting
+ */
+ 
+error_reporting(E_ALL);
+
+define('APPLICATION_DIR', 'application');
+
+// --------------------------------------------------------------------------
+
+/**
+ * Define some system-level constants
+ */
+
+define('EXT', '.php');
+define('SYSTEM_DIR', 'system');
+
+/**
+ * And away we go...
+ */
+ 
+require_once SYSTEM_DIR . '/n2/n2' . EXT;
+
+ 
+/* End of file index.php */
+/* Location: ./index.php */
---
+++ system/n2/n2.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * N2
+ * 
+ * @license		CC BY-SA 3.0
+ * @author		Chris Atkin
+ * @link		http://chrisatk.in/
+ * @email		contact {at} chrisatk {dot} in
+ * 
+ * @file		n2.php
+ * @version		1.0
+ * @date		05/26/2011
+ * 
+ * Copyright (c) 2011 Chris Atkin. All rights reserved.
+ */
+ 
+namespace n2;
+ 
+/* End of file n2.php */
+/* Location: ./system/n2/n2.php */
